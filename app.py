ef113a9ddb2fe38bc83dd3882c1d2f0cc7ba507a
Segun Oladipo <71088203+SegunDave@users.noreply.github.com>
2023-01-26 00:51:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -285,7 +285,7 @@ st.sidebar.markdown(
     -----------
     # Let's connect
  
-    [![Segun Oladipo](https://img.shields.io/badge/Author-@VictorOgunjobi-gray.svg?colorA=gray&colorB=dodgergreen&logo=github)](https://github.com/SegunDave)
+    [![Segun Oladipo](https://img.shields.io/badge/Author-@SegunOladipo-gray.svg?colorA=gray&colorB=dodgergreen&logo=github)](https://github.com/SegunDave)
     [![Segun Oladipo](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logoColor=white)](https://www.linkedin.com/in/segun-oladipo-44174796/)
     [![Segun Oladipo](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=gray)](https://twitter.com/Segunoladeepo)
     """)
